1955a88eea778ba4ff520a2de59ac039c2d36edd
NicoleMwebembezi <nicolemwebembezi18@gmail.com>
2026-03-19 18:15:32 +0300

    AuthService Function

--- clinic_system/services/auth_service.py
+++ clinic_system/services/auth_service.py
@@ -1,1 +1,11 @@
 
+class AuthService:
+    def __init__(self, storage):
+        self.storage = storage
+        self.users = self.storage.load()
+
+    def login(self, username, password):
+        for user in self.users:
+            if user["username"] == username and user["password"] == password:
+                return user
+        return None

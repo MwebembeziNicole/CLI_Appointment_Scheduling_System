1c6398c42391b5744d6128bc29f6de4d36ae816f
NicoleMwebembezi <nicolemwebembezi18@gmail.com>
2026-03-19 18:13:34 +0300

    patient fields

---
+++ clinic_system/models/patient.py
@@ -0,0 +1,7 @@
+class Patient:
+    def __init__(self, patient_id, name, age, gender, contact):
+        self.patient_id = patient_id
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.contact = contact

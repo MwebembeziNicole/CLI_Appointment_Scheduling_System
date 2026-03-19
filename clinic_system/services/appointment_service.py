8fec5344f8b5744d444b4e2849e099d6966326ca
NicoleMwebembezi <nicolemwebembezi18@gmail.com>
2026-03-19 18:15:02 +0300

    AppointmentService function

--- clinic_system/services/appointment_service.py
+++ clinic_system/services/appointment_service.py
@@ -1,1 +1,18 @@
 
+class AppointmentService:
+    def __init__(self, storage):
+        self.storage = storage
+        self.appointments = self.storage.load()
+
+    def schedule(self, patient_id, doctor, date, time):
+        appointment = {
+            "patient_id": patient_id,
+            "doctor": doctor,
+            "date": date,
+            "time": time
+        }
+        self.appointments.append(appointment)
+        self.storage.save(self.appointments)
+
+    def get_all(self):
+        return self.appointments

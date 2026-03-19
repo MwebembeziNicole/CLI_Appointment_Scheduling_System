7d600a8da5f8df659abd4aad55030c406fd8c3b9
NicoleMwebembezi <nicolemwebembezi18@gmail.com>
2026-03-19 18:13:07 +0300

    Appointment fields

---
+++ clinic_system/models/appointment.py
@@ -0,0 +1,6 @@
+class Appointment:
+    def __init__(self, patient_id, doctor, date, time):
+        self.patient_id = patient_id
+        self.doctor = doctor
+        self.date = date
+        self.time = time
